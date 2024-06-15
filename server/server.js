a8f9f10217638129191cf6192b1bb02f3b663c50
Lauren <laurenfelty01@gmail.com>
2024-06-15 12:01:04 -0500

    update port for front end

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-// const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser')
-// const path = require('path');
-// const cors = require('cors');
-// const app = express();
-// const port = 3000;
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
 // Middleware for parsing JSON bodies
-// app.use(express.json());
-// app.use(bodyParser.json())
-// app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-// app.use(express.static('../public'));
+app.use(express.static('../public'));
 
-// app.listen(port, () => {
-//     console.log(`Server running on http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
