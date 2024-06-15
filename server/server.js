7100a3d56df59cbd53698ea96208c26e483968ef
Lauren <laurenfelty01@gmail.com>
2024-06-15 14:29:21 -0500

    update webpck

--- server/server.js
+++ server/server.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(express.static('../public'));
+// Parse incoming URL data
+app.use(express.urlencoded({ extended: true }));
+
+// Accept incoming requests from the front end which is on another server
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+  })
+);
+
+// Serve static files from the dist directory
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
