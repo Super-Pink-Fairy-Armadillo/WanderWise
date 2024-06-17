a317aad6cdde51347f3ed8c01232c4aa9e6e467f
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-06-17 09:10:24 -0700

    dashboard+trip from fred

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
 import Trip from "./components/Trip.jsx";
-import './sass/application.scss';
+// import './sass/application.scss';
+import './sass/dashStyles.css';
 
 // using routes instead of switch
 function App() {
   return (
     <BrowserRouter>
       <Routes> 
-        <Route path="/:user" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         {/* endpoint / for dashboard, user is userId */}
         {/* on login/signin submit, navitage to endpoint with userid like this ^ */}
-        <Route path="/trip/:tripId" element={<Trip />} />
+        <Route path="/trip" element={<Trip />} />
         {/* passing in tripId when clicking on component */}
       </Routes>
     </BrowserRouter>
--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const TripCard = ({ tripInfo }) => {
-    const { _id, tripName, tripLocation, startDate, endDate } = tripInfo;
+    const {tripName, tripLocation, startDate, endDate } = tripInfo;
     const formattedStart = new Date(startDate).toLocaleDateString();
     const formattedEnd = new Date(endDate).toLocaleDateString();
 
+    const navigate = useNavigate();
     const handleClick = () => {
-        useNavigate(`/trip/${_id}`)
+        navigate(`/trip`)
     }
     
     return (
@@ -17,5 +19,23 @@ const TripCard = ({ tripInfo }) => {
         </div>
     );
 };
+
+// const TripCard = ({ tripInfo }) => {
+//     const { _id, tripName, tripLocation, startDate, endDate } = tripInfo;
+//     const formattedStart = new Date(startDate).toLocaleDateString();
+//     const formattedEnd = new Date(endDate).toLocaleDateString();
+
+//     const handleClick = () => {
+//         useNavigate(`/trip/${_id}`)
+//     }
+    
+//     return (
+//         <div class='tripCard' onClick={handleClick}>
+//         <h2 class='tripCardName'>{tripName}</h2>
+//         <p class='tripCardLocation'>{tripLocation}</p>
+//         <p class='tripCardDates'>{formattedStart} - {formattedEnd}</p>
+//         </div>
+//     );
+// };
 
 export default TripCard;
