944b9b0eca5b625822f0b4ad0af2a3bab3373b5a
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-06-17 07:22:01 -0700

    update app/index to use router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
 import Trip from "./components/Trip.jsx";
+import './sass/application.scss';
 
 // using routes instead of switch
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from './components/Dashboard.jsx';
-//import App.jsx and delete the app function above later
-//import styles to app.jsx when moved
-import './sass/application.scss'
+import App from './App.jsx';
 
-function App() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
